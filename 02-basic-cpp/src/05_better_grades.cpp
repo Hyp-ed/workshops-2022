dff7968ebda78039f839c05d0634c254860b1e2f
Franz Miltz <franz@miltz.me>
2021-09-24 19:39:10 +0100

    WS02: Exercise 5

---
+++ 02-basic-cpp/src/05_better_grades.cpp
@@ -0,0 +1,20 @@
+#include "05_better_grades.hpp"
+
+namespace hyped::workshop {
+
+Grade scoreToGrade(const int score)
+{
+  return Grade::kInvalid;
+}
+
+Grade stringToGrade(const std::string str)
+{
+  return Grade::kInvalid;
+}
+
+std::string gradeToString(const Grade grade)
+{
+  return "Dummy Grade";
+}
+
+}  // namespace hyped::workshop
